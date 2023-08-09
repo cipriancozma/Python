50cf1b2bb89efbff5beb51f311492da47974fd17
Ciprian <cozma_ciprian19@yahoo.com>
2023-08-09 23:50:50 +0300

    refactored caesar py file

--- caesar_cipher/caesar.py
+++ caesar_cipher/caesar.py
@@ -1,7 +1,10 @@
+from logo import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -10,6 +13,9 @@ shift = int(input("Type the shift number:\n"))
 def caesar(text, shift, direction):
     newString = ''
     for letter in text:
+        if shift > 26:
+            shift = shift % alphabet.index(letter)
+            print(shift)
         oldString = alphabet.index(letter)
         if direction == 'encode':
             newStringIdx = oldString + shift
@@ -19,3 +25,14 @@ def caesar(text, shift, direction):
     print(f"The {'encoded' if direction == 'encode' else 'decoded'} text is {newString}")
 
 caesar(text, shift, direction)
+
+user_response = input("Do you want to restart the program?")
+
+while user_response == 'yes':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, direction)
+    user_response = input("Do you want to restart the program?")
+
+
