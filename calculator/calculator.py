fa0670b5ddb7796f57e8711b6042882bb34a77e5
Ciprian <cozma_ciprian19@yahoo.com>
2023-08-17 23:12:13 +0300

    added calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,33 @@
+from logo import logo
+
+print(logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def substract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for key in operations:
+    print(key)
+operation_symbol = input("Pick an operation from the line above: ")
+
+answer = operations[operation_symbol](num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
