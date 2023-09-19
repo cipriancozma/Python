1191e2273573b718c7665a9bc430669c6af94208
Ciprian <cozma_ciprian19@yahoo.com>
2023-09-19 23:17:51 +0300

    updated fundamentals py file

--- basics/fundamentals.py
+++ basics/fundamentals.py
@@ -12,6 +12,14 @@ WWW
 '''
 # print(long_string)
 
+# bool
+isValid = True
+# print(type(isValid))
+
+# list
+li = [1, 2, 3, 5, True, 'a']
+# print(li)
+
 # math functions
 # print(round(3.2))
 # print(abs(-3))
@@ -25,4 +33,38 @@ WWW
 
 # binary
 # print(bin(256))
+
+# string indexes
+selfish = '01234567'
+# print(selfish[1:8:1])
+
+#methods
+# print(len('helloooo'))
+
+quote = 'to be or not to be'
+# print(quote.upper())
+# print(quote.capitalize())
+# print(quote.replace('be', 'me'))
+
+# list methods
+cart = ['notebooks', 'toys', 'grapes', 'hello']
+# print(cart[0:2])
+
+basket = [1, 2, 3, 4, 5]
+
+# adding
+basket.append(6)
+# print(basket)
+basket.insert(2, 8)
+# print(basket)
+basket.extend([200])
+# print(basket)
+
+# removing
+basket.pop()
+# print(basket)
+basket.remove(6)
+# print(basket)
+# basket.clear()
+# print(basket)
 
